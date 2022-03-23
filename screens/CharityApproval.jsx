e89ba84a9af9183d15349184c0ea909b624dbba7
Arif Uddin <arifunctg@gmail.com>
2022-03-23 06:04:04 +0600

    Update CharityApproval.jsx

--- screens/CharityApproval.jsx
+++ screens/CharityApproval.jsx
@@ -1,51 +1,40 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { Text, View, StyleSheet, ScrollView } from "react-native";
 import { Appbar, Button, IconButton } from "react-native-paper";
-import ListCard from "../components/ListCard";
 import TopBar from "../components/TopBar";
-const ch = [
-  {
-    title: 'The Human Foundation',
-    subTitle: 'Help homless and poor family, and help the community',
-    img: 'https://cdn.downtoearth.org.in/uploads/0.51881800_1447151770_poverty.jpg'
-  },
-  {
-    title: 'Feed the Babies',
-    subTitle: 'Help homless and poor family, and help the community',
-    img: 'https://images.squarespace-cdn.com/content/v1/55caa6b6e4b0b9a7ca485045/1479410086119-AZ8FSHFS6MRXUDJ3JEC6/image-asset.jpeg?format=500w'
-  },
-  {
-    title: 'Save the Community',
-    subTitle: 'Help homless and poor family, and help the community',
-    img: 'https://image.shutterstock.com/image-vector/people-throw-gold-coins-into-260nw-1336223207.jpg'
-  },
-  {
-    title: 'Sisters of Charity',
-    subTitle: 'Help homless and poor family, and help the community',
-    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzbqCI0X0NTidPitnBa0XJwqiSMnvWes5ZIw&usqp=CAU'
-  },
-  {
-    title: 'Helping Hands Club',
-    subTitle: 'Help homless and poor family, and help the community',
-    img: 'https://thumbs.dreamstime.com/b/concept-charity-food-poor-life-problems-hunger-society-helping-people-kindness-hands-beggars-receive-donated-149442931.jpg'
-  },
-  {
-    title: 'The Funding Network',
-    subTitle: 'Help homless and poor family, and help the community',
-    img: 'https://i.ytimg.com/vi/2rTZ9UndNeI/maxresdefault.jpg'
-  },
-  {
-    title: 'The Funding Network',
-    subTitle: 'Help homless and poor family, and help the community',
-    img: 'https://www.lovesove.com/wp-content/uploads/2021/04/Laughter-Day-Messages-Lovesove.jpg'
-  }
-]
+import { db } from "../firebaseConfig";
+import { useState, useEffect } from "react";
+import StatusCard from "../components/StatusCard";
+
 export default function CharityApproval() {
+  const [charities, setCharities] = useState([])
+  const [refetchData, setRefetchData] = useState(false)
+  useEffect(async () => {
+    const data = []
+    const usersDetails = await getDocs(collection(db, 'userDetails'))
+    usersDetails.forEach(doc => {
+      data.push({ ...doc.data(), id: doc.id })
+    })
+    setCharities(data)
+  }, [refetchData])
+
   return (
     <>
-      <TopBar title="Charity List" />
+      <TopBar title="Charity Status List" />
       <ScrollView style={{ flex: 1 }}>
-        {ch.map((itm, i) => (
-          <ListCard key={`lc-${i + 9}`} img={itm.img} title={itm.title} subTitle={itm.subTitle} />
+        {charities.map((itm, i) => (
+          <StatusCard
+            key={`lc-${i + 9}`}
+            img={itm.img}
+            title={itm.name}
+            subTitle={itm.slogan}
+            status={itm.status}
+            description={itm.description}
+            location={itm.location}
+            phone={itm.phone}
+            id={itm.id}
+            action={() => setRefetchData(!refetchData)}
+          />
         ))}
       </ScrollView>
     </>
